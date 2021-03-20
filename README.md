e0fb8b68323490831da66097fb425ad3e2622465
Michel Bretschneider <embix@gmx.net>
2021-03-20 17:09:44 +0100

    +initial version

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # RollingLineSaveGameFixer
 
 try fix corrupted RollingLine save game files by loading them without wagons and then saving them again.
+
+## Build
+
+Use either visual studio or dotnet command line as follows:
+
+```
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true
+```
+
+In case you want to target another platform use win-x32, linux-64, osx-64 or whatever
+
+You find the build output in `bin\Debug\net5.0\$PLATFORM\publish`.
---
+++ src/RollingLineSaveGameFixer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace RollingLineSaveGameFixer
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            if (args.Length!=1)
+            {
+                PrintUsage();
+                return;
+            }
+            TryFixFile(args[0]);
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: RollingLineSaveGameFixer filename");
+        }
+
+        static void TryFixFile(String fullFileName)
+        {
+            var rawSave = File.ReadAllText(fullFileName);
+            // false: don't load wagons - that's usually the messed part of the savegame
+            var savegameObj = new GameSave(rawSave,false);
+            if (savegameObj.sucessfullLoad)
+            {
+                File.WriteAllText(fullFileName+".backup", rawSave);
+                var fixedContent = savegameObj.Save(true);
+                File.WriteAllText(fullFileName, fixedContent);
+                Console.WriteLine("hopefully fixed the file, also created backup");
+            }
+            else
+            {
+                Console.WriteLine("Sorry: could not even load without wagons :-(");
+            }
+        }
+    }
+}
